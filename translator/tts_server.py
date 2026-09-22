c1e1de260205ed238fb84e9956a5155b371644e6
Mrunal Gaikwad <mrunal26@gmail.com>
2026-09-22 23:59:58 +0530

    Prepare TTS server for Render

--- translator/tts_server.py
+++ translator/tts_server.py
@@ -330,8 +330,11 @@ def text_to_speech():
 # ==============================
 
 if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get("PORT", 5002))
 
     app.run(
-        host="127.0.0.1",
-        port=5002
+        host="0.0.0.0",
+        port=port
     )
